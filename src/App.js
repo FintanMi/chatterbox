741557a80a2ff5855900f06ac6fb259ffb7c2fbf
FintanMi <>
2023-11-16 17:48:34 +0000

    added navbar and register form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-import './App.css';
+import NavBar from './components/NavBar';
+import styles from './App.module.css';
+import { Container } from 'react-bootstrap';
+import { Switch, Route } from 'react-router-dom';
+import './api/axiosDefault';
+import Register from './pages/auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.MainContent}>
+        <Switch>
+            <Route exact path='/login' render={() => <p>login</p>} />
+            <Route exact path='/register' render={() => <Register />} />
+            <Route render={() => <p>Page not found</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
